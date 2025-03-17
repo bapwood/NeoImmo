4906517aec3bfa923cd426a8b067b19abc8a4a6f
Baptiste <baptiste.fouillet@epitech.eu>
2025-03-17 13:08:51 +0100

    feat: added first pages for frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@mui/material';
+import * as React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>
+        ouaaaiiii
+      </Button>
+      <img
+        src="NeoImmo_logo_nobg.png"
+        className="bg-cover bg-red-50 scale-50"
+      >
+
+      </img>
+    </div>
+  );
+}
