d3ab0d070d798a52294fab813be58d93ca3db68c
Ismaiel Diapaka <ismaiel.diapaka@epitech.eu>
2025-12-13 00:22:50 +0300

    fix: docker compose build error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.DATABASE_URL);
 }
 bootstrap();
