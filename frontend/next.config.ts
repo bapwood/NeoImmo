c7db0a946582629a54811384fde9741cafda7182
baptiste fouillet <baptiste.fouillet@epitech.eu>
2026-06-26 01:33:48 -0500

    feat: modify behaviour of vercel proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       beforeFiles: [
         {
           source: "/api/:path*",
-          destination: "http://TON_IP_AWS:TON_PORT/api/:path*",
+          destination: "http://18.224.104.1:3001/api/:path*",
         },
       ],
     };
